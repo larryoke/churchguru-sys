69a71c1ac6732a75cdae971f22b4285a77bdd89c
root <root@v01.27860.vpscontrol.net>
2017-03-05 04:58:47 +0400

    update ssl stuff

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 rm certificate17-18.pfx
-rm  ~/.churchguru-deploy/keystore.jks 
+rm  keystore.jks 
 sleep 3
 
 echo "openssl"
-openssl pkcs12 -export -out certificate17-18.pfx -inkey faithchapel.churchg.com.key -in faithchapel.churchg.com.crt -certfile faithchapel.churchg.com.ca
+openssl pkcs12 -export -out certificate17-18.pfx -inkey faithchapel.churchg.com.key -in faithchapel.churchg.com.crt -certfile faithchapel.churchg.com.ca  -name "tomcat"
 sleep 3
 
 
 echo "keytool -genkey"
-keytool -genkey -alias tomcat -keystore ~/.churchguru-deploy/keystore.jks
+keytool -importkeystore -srckeystore certificate17-18.pfx -srcstoretype pkcs12 -srcalias tomcat  -destkeystore keystore.jks -deststoretype JKS  -destalias tomcat
 sleep 3 
 
 
 echo "keytool -importkeystore"
-keytool -importkeystore -srckeystore ~/.churchguru-deploy/keystore.jks -destkeystore ~/.churchguru-deploy/keystore.p12 -srcstoretype JKS -deststoretype PKCS12 -deststorepass larryoke -srcalias tomcat -destalias tomcat
+keytool -importkeystore -srckeystore keystore.jks -destkeystore keystore.p12 -srcstoretype JKS -deststoretype PKCS12 -srcstorepass larryoke -deststorepass larryoke -srcalias tomcat -destalias tomcat -srckeypass larryoke -destkeypass larryoke -noprompt
